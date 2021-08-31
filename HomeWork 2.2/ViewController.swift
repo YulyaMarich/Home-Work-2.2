8c0800319abe1bfb108cad933e053d6be758dd30
Yulia Marych <yulyamarich@icloud.com>
2021-08-31 17:34:37 +0300

    Home Work 2.2

--- HomeWork 2.2/ViewController.swift
+++ HomeWork 2.2/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
-
-
+    
+    private func changeViewColor() {
+        let redColor = CGFloat(redSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    @IBAction func changeRedSliderValue() {
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        changeViewColor()
+    }
+    
+    @IBAction func changeGreenSliderValue() {
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        changeViewColor()
+    }
+    
+    @IBAction func changeBlueSliderValue() {
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        changeViewColor()
+    }
 }
 
