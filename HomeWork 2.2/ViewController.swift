6e76ae456b6153dddbc97a1745dad238b8788caa
Yulia Marych <yulyamarich@icloud.com>
2021-09-01 12:11:05 +0300

    Color change app with sliders

--- HomeWork 2.2/ViewController.swift
+++ HomeWork 2.2/ViewController.swift
@@ -21,36 +21,50 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        redSliderValue.text = String(format: "%.2f", redSlider.value)
-        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
-        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        initialSetupUI()
+        changeViewColor()
+    }
+    
+   
+    @IBAction func changeRedSliderValue() {
+        redSliderValue.text = string(redSlider)
+        changeViewColor()
+    }
+    
+    @IBAction func changeGreenSliderValue() {
+        greenSliderValue.text = string(greenSlider)
+        changeViewColor()
+    }
+    
+    @IBAction func changeBlueSliderValue() {
+        blueSliderValue.text = string(blueSlider)
+        changeViewColor()
+    }
+
+
+    private func initialSetupUI() {
+        redSliderValue.text = string(redSlider)
+        greenSliderValue.text = string(greenSlider)
+        blueSliderValue.text = string(blueSlider)
         
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
     }
     
+    private func string(_ slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    
     private func changeViewColor() {
         let redColor = CGFloat(redSlider.value)
         let blueColor = CGFloat(blueSlider.value)
         let greenColor = CGFloat(greenSlider.value)
         
-        colorView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
-    }
-    @IBAction func changeRedSliderValue() {
-        redSliderValue.text = String(format: "%.2f", redSlider.value)
-        changeViewColor()
-    }
-    
-    @IBAction func changeGreenSliderValue() {
-        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
-        changeViewColor()
-    }
-    
-    @IBAction func changeBlueSliderValue() {
-        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
-        changeViewColor()
+        colorView.backgroundColor = UIColor(red: redColor,
+                                            green: greenColor,
+                                            blue: blueColor,
+                                            alpha: 1)
     }
 }
-
